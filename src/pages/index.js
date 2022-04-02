f820b0128f3f08ac6c6ea080b673d9dea53c9ea4
Joe DeMilo <jdub@Joes-MacBook-Air.local>
2022-04-02 15:42:55 -0400

    progress

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,90 +1,129 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import PropTypes from 'prop-types';
-import ReactMarkdown from 'react-markdown/with-html';
 
-import Seo from '@/components/SEO/Seo';
+// import Seo from '@/components/SEO/Seo';
 import Layout from '@/components/Layout';
 
-import Logo from '@/icons/Logo';
+import Link from 'gatsby-link';
+
+import Card from '../components/Card';
+import Sidebar from '../components/Sidebar';
+import ShareBox from '../components/ShareBox';
+
+import '../templates/index.scss';
+
+// import Logo from '@/icons/Logo';
+
+const NavLinkText = ({ color, text }) => (
+  <div
+    className="navlink"
+    style={{
+      color,
+    }}
+  >
+    {text}
+  </div>
+);
+
+const NavLink = ({ test, url, text }) => {
+  if (!test) {
+    return <NavLinkText color="#7d7d7d" text={text} />;
+  }
+
+  return (
+    <Link to={`${url}`}>
+      <NavLinkText color="#66ccff" text={text} />
+    </Link>
+  );
+};
 
 const HomePageTemplate = ({ data }) => {
+  // const { edges: group } = data.allMarkdownRemark
+
   return (
     <>
-      <div className='min-h-screen flex items-center justify-center dark:bg-zinc-900'>
-        <div className='container px-5 sm:px-10'>
-          <section className='w-full py-20 px-5 rounded-lg shadow border-grey-lighter dark:border-zinc-600 border'>
-            <div className='text-center mx-auto'>
-              <div>
-                <Logo className='mx-auto h-8 w-auto dark:text-white' />
-              </div>
-              <p className='text-xs mt-2 text-black dark:text-white'>
-                {data.version}
-              </p>
+      <div
+        className="row homepage"
+        style={{
+          marginTop: 20,
+        }}
+      >
+        <Sidebar />
+        <div className="col-xl-6 col-lg-7 col-md-12 col-xs-12 order-2">
+          {data.map(({ node }) => (
+            <Card {...data} url={data.slug ? data.slug : node.fields.slug} key={node.fields.slug} />
+          ))}
+
+          <div
+            className="row"
+            style={{
+              justifyContent: 'space-around',
+              marginBottom: '20px',
+            }}
+          >
+            {/* <div className="previousLink">
+              <NavLink test={!first} url={previousUrl} text="Previous" />
             </div>
-            <ReactMarkdown
-              children={data.description}
-              className='text-center mt-6 dark:text-white'
-              allowDangerousHtml
-            />
-            {data.links.length > 0 ? (
-              <div className='flex justify-center flex-wrap items-center mt-6'>
-                {data.links.map(({ link }, i) => {
-                  return (
-                    <a
-                      href={link.url}
-                      key={i}
-                      target='_blank'
-                      rel='noreferrer'
-                      className='text-sm m-2 dark:text-white'>
-                      {link.content}
-                    </a>
-                  );
-                })}
-              </div>
-            ) : (
-              ''
-            )}
-          </section>
+            <div className="nextLink">
+              <NavLink test={!last} url={nextUrl} text="Next" />
+            </div> */}
+          </div>
         </div>
+        <div className="col-xl-2 col-lg-1 order-3" />
       </div>
+      {/* <ShareBox url={location.href} hasCommentBox={false} /> */}
     </>
   );
 };
 
 const HomePage = ({ data }) => {
-  const { frontmatter } = data.markdownRemark;
+  //  const { frontmatter } = data.allMarkdownRemark;
 
+  const { edges: frontmatter } = data.allMarkdownRemark;
+
+  console.log(JSON.stringify(frontmatter));
   return (
     <Layout>
-      <Seo data={frontmatter.seo} />
+      {/* <Seo data={frontmatter.seo} /> */}
       <HomePageTemplate data={frontmatter} />
     </Layout>
   );
 };
 
-HomePage.propTypes = {
-  data: PropTypes.shape({
-    markdownRemark: PropTypes.shape({
-      frontmatter: PropTypes.object,
-    }),
-  }),
-};
+// HomePage.propTypes = {
+//   data: PropTypes.shape({
+//     markdownRemark: PropTypes.shape({
+//       frontmatter: PropTypes.object,
+//     }),
+//   }),
+// };
 
 export const pageQuery = graphql`
-  query HomePageTemplate {
-    markdownRemark(frontmatter: { template: { eq: "index" } }) {
-      frontmatter {
-        title
-        version
-        description
-        links {
-          link {
-            content
-            url
+  query HomePageTemplate  {
+    allMarkdownRemark(
+      limit: 1000
+      sort: { order: DESC, fields: frontmatter___date }
+    ) {
+      edges {
+        node {
+          id
+          fields {
+            slug
+          }
+          frontmatter {
+            tags
+            template
+            slug
+            id
+            title
+            url: slug
+            date
+            tags
+            description
+            headerImage
           }
         }
-        ...SEO
       }
     }
   }
